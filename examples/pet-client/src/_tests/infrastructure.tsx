15e9befe3e65dfea879599816389e92db56a4960
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-09-26 08:38:44 +0700

    Add header to autogenerated file

--- examples/pet-client/src/_tests/infrastructure.tsx
+++ examples/pet-client/src/_tests/infrastructure.tsx
@@ -4,12 +4,10 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import App from '../App';
 import { QueryFactory, QueryFactory2 } from '../api';
 import { setBaseUrl } from './nock-helpers';
-import { setBaseUrl as setBaseUrl2 } from './nock-helpers2';
 import axios from 'axios';
 
 axios.defaults.adapter = require('axios/lib/adapters/http');
 setBaseUrl('https://localhost');
-setBaseUrl2('https://localhost');
 
 QueryFactory.setBaseUrl('https://localhost');
 QueryFactory.setAxiosFactory(() => axios);
